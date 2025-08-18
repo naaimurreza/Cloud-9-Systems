ebda839e600b6421fe608d9b80e82893c5fa7a8b
Naaimur <naaimur7@gmail.com>
2025-08-18 15:22:30 +0600

    add visual improvements

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <conio.h>
 #include <windows.h>
 #define NUM_BUCKETS 1000
+#define TAX_RATE 0.12
 
 
 struct product {
@@ -67,7 +68,7 @@ int main() {
                 case 4:
                     if(display_orders()) {
                         int order_num;
-                        printf("Select order to edit: ");
+                        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tSelect order to edit: ");
                         scanf("%d", &order_num);
                         Order *order_ptr = &orders[order_num - 1];
                         edit_order(order_ptr);
@@ -85,7 +86,7 @@ int main() {
                 }
             }
         } else {
-            printf("\n\nLogin failed! Please check your username and password and try again.\n\n");
+            printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\tLogin failed! Please check your username and password and try again.\n\n");
             login();
         }
     return 0;
@@ -106,11 +107,41 @@ int login() {
     file = fopen("login.txt", "r");
     fscanf(file, "%s\n%s", saved_username, saved_password);
     fclose(file);
+                                                                 
+                                                                                      
+    printf("\t\t\t\t\t\t\t\t\t\t                                     @@@@@@                                     \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                                 @@@@@@@@@@@@@@                                 \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                               @@@@@@@@@@@@@@@@@@                               \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                             @@@@@@@      @@@@@@@                              \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                             @@@@@@          @@@@@@                             \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                        @@@@@@@@@@   @@@@@@   @@@@@                             \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                     @@@@@@@@@@@@   @@@@@@@}  @@@@@@@@@@                        \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                    @@@@@@@@@@@@@@   @@@@@@   @@@@@@@@@@@@@                     \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                   @@@@@@@@@@@@@@@     @@     @@@@@@@@@@@@@@@                   \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                 @@@@@@@@@@@@@@@@@@@        @@@@@@@@@@@@@@@@@                   \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                @@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@                  \n");
+    printf("\t\t\t\t\t\t\t\t\t\t               @@@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@                  \n");
+    printf("\t\t\t\t\t\t\t\t\t\t               @@@@@@@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@@@@@@@                   \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*                     \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                                                                                \n");
+    printf("\t\t\t\t\t\t\t\t\t\t              @@@@@@@   @@                            @@   @@@@@@              \n");
+    printf("\t\t\t\t\t\t\t\t\t\t              @@@@ @@@@@ @@~                           @@  @@@ .@@@             \n");
+    printf("\t\t\t\t\t\t\t\t\t\t             @@@         @@  @@@@@@@@  @@   @@@  @@@@@@@@ @@@    @@@            \n");
+    printf("\t\t\t\t\t\t\t\t\t\t            @@@          @@ @@@    @@@ @@   @@@ @@@   @@@  @@@@@@@@@            \n");
+    printf("\t\t\t\t\t\t\t\t\t\t             @@@      +  @@ @@@    @@@ @@   @@@ @@@    @@   )@@@@@@             \n");
+    printf("\t\t\t\t\t\t\t\t\t\t              @@@@ @@@@@ @@  @@@  @@@  @@@ ~@@@ @@@@  @@@     @@@@              \n");
+    printf("\t\t\t\t\t\t\t\t\t\t               @@@@@@@   @@   @@@@@@    @@@@@@@   @@@@@@@   @@@@                \n");
+    printf("\t\t\t\t\t\t\t\t\t\t                                                                                \n");
+    printf("\t\t\t\t\t\t\t\t\t\t               @  @@  @@ @@  @@ @@   @@@   @@     @@@  @@@  @@ @@               \n");
+    printf("\t\t\t\t\t\t\t\t\t\t               =@@@@   @@@    @@@@    @#   @@@@@  @@@@@@@@   @@@@               \n");
+    printf("\t\t\t\t\t\t\t\t\t\t               @@@@@    @    @@@@@   +@@   @@@@@  @@  @ @@  @@@@@               \n\n");
 
-
-    printf("Welcome to Cloud 9 Systems! \n\n");
-    printf("Login: \n");
-    printf("Username: ");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t_______________________________\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| Welcome to Cloud 9 Systems! |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t|*****************************|\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| Login:                      |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t  Username: ");
     scanf("%s", username);
     fflush(stdin);
 
@@ -119,7 +150,7 @@ int login() {
     GetConsoleMode(hStdin, &mode);
     SetConsoleMode(hStdin, mode & (~ENABLE_ECHO_INPUT));
 
-    printf("Password: ");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t  Password: ");
     int i = 0;
     char ch;
 
@@ -139,13 +170,13 @@ int login() {
 
     file = fopen("login.txt", "r");
     if (file == NULL) {
-        printf("Error opening file!\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tError opening file!\n");
         return 1;
     }
     
 
     if(strcmp(username, saved_username) == 0 && hash(password) == atoi(saved_password)) {
-        printf("\n\nLogin successful!\nHello, %s!\n\n", username);
+        printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\tLogin successful!\n\t\t\t\t\t\t\t\t\t\t\t\t\tHello, %s!\n\n", username);
         return 1;
     } else {
         return 0;
@@ -190,13 +221,13 @@ void write_orders(Order orders[100]) {
 }
 
 int display_orders() {
-    printf("\nDisplaying all orders: \n\n");
+    printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tDisplaying all orders: \n\n");
     if (order_count == 0) {
-        printf("No orders found.\n\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tNo orders found.\n\n");
         return 0;
     } else {
         for (int j = 0; j < order_count; j++) {
-            printf("%d  - Name: %s,  Order ID: %ld, Total Price: $%.2f, Product: ",
+            printf("\t\t\t\t\t\t\t\t\t\t\t%d  - Name: %s,  Order ID: %ld, Total Price: $%.2f, Product: ",
             j+1, orders[j].username, orders[j].order_id, orders[j].product.price);
             printf("%s  ", orders[j].product.name);
             printf("\n");
@@ -207,15 +238,15 @@ int display_orders() {
 }
 
 void display_products() {
-    printf("Available Products:\n\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tAvailable Products:\n\n");
     for (int i = 0; i < sizeof(products) / sizeof(products[0]); i++) {
-        printf("%d. %s - TK. %.2f\n", i + 1, products[i].name, products[i].price);
-        printf("\n   Features:\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t%d. %s - TK. %.2f\n", i + 1, products[i].name, products[i].price);
+        printf("\n\t\t\t\t\t\t\t\t\t\t\t\t   Features:\n");
         for (int j = 0; j < 4; j++) {
             if (strlen(products[i].features[j]) == 0) {
                 break;
             }
-            printf("   - %s\n", products[i].features[j]);
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t   - %s\n", products[i].features[j]);
         }
         printf("\n\n");
     }
@@ -223,27 +254,31 @@ void display_products() {
 }
 
 void display_menu() {
-    printf("Main Menu\n");
-    printf("1. Display all orders\n");
-    printf("2. Create order\n");
-    printf("3. Search from orders\n");
-    printf("4. Edit existing order\n");
-    printf("5. Delete order\n");
-    printf("6. Exit\n");
-    printf("Please select an option: ");
+    printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\t+----------------------------------------+\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t|        Cloud 9 Systems CLI Menu        |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+----------------------------------------+\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| 1. Display all orders                  |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| 2. Create order                        |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| 3. Search from orders                  |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| 4. Edit existing order                 |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| 5. Delete order                        |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| 6. Exit                                |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+----------------------------------------+\n");
+
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tPlease select an option: ");
 }
 
 void delete_order() {
     if(order_count == 0) {
-        printf("\nNo orders to delete.\n\n");
+        printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tNo orders to delete.\n\n");
         return;
     }
     display_orders();
-    printf("Enter the order number to delete: ");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tEnter the order number to delete: ");
     int order_number;
     scanf("%d", &order_number);
     if (order_number < 1 || order_number > order_count) {
-        printf("Invalid order number.\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tInvalid order number.\n");
         delete_order();
         return;
     } else {
@@ -252,24 +287,51 @@ void delete_order() {
         }
         order_count--;
         write_orders(orders);
-        printf("\nOrder deleted successfully.\n\n");
+        printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tOrder deleted successfully.\n\n");
     }
 }
 
+void print_receipt(Order new_order) {
+    float subtotal = 0.0, tax, total;
+
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+-----------------------------------------------+\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t|           Cloud 9 Systems Receipt             |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+-----------------------------------------------+\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| Item              Qty   Unit    Total         |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+-----------------------------------------------+\n");
+
+    
+    subtotal =new_order.product.price;
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| %-17s 1  $%7.2f  $%7.2f       |\n",
+            new_order.product.name, new_order.product.price, subtotal);
+ 
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+-----------------------------------------------+\n");
+
+    tax = subtotal * TAX_RATE;
+    total = subtotal + tax;
+
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| Subtotal:                        $%7.2f     |\n", subtotal);
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| Tax (12%%):                       $%7.2f     |\n", tax);
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t| TOTAL:                           $%7.2f     |\n", total);
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+-----------------------------------------------+\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t|     Thank you for shopping with us!           |\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t+-----------------------------------------------+\n\n");
+}
+
 void make_order() {
-    printf("\n\nCreating order...\n\n");
+    printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\tCreating order...\n\n");
     display_products();
-    printf("Please select a product by number: ");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tPlease select a product by number: ");
     int product_choice;
     scanf("%d", &product_choice);
     if (product_choice < 1 || product_choice > 5) {
-        printf("Invalid product selection. Try again.\n");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tInvalid product selection. Try again.\n");
         make_order();
     } else {
         char username[50];
-        printf("Enter Name: ");
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tEnter Name: ");
         scanf("%s", username);
-        printf("You selected: %s - $%.2f\n", products[product_choice - 1].name, products[product_choice - 1].price);
+        printf("\t\t\t\t\t\t\t\t\t\t\t\t\tYou selected: %s - $%.2f\n", products[product_choice - 1].name, products[product_choice - 1].price);
         Order new_order = default_order;
         strcpy(new_order.username, username);
         new_order.order_id = rand() % 1000000;
@@ -281,31 +343,30 @@ void make_order() {
             fprintf(fp,"Name: %s\nOrder ID: %d\n", new_order.username, new_order.order_id);
             fprintf(fp,"Product:\nName: %s\nPrice: %.2f\n\n", new_order.product.name, new_order.product.price); 
         } else {
-            printf("\nError opening file.\n");
+            printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tError opening file.\n");
         }
         fclose(fp);
-        printf("\nOrder created successfully!\n\n");
-        printf("Name: %s\n", new_order.username);
-        printf("Order ID: %ld\n", new_order.order_id);
-        printf("Product in order:\n");
-        printf("%s - $%.2f\n\n", new_order.product.name, new_order.product.price);
+        printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tOrder created successfully!\n\n");
+
+        print_receipt(new_order);
+
     }
 }
 
 void display_order(int order_num) {
-    printf("Name: %s\n", orders[order_num].username);
-    printf("Order ID: %ld\n", orders[order_num].order_id);
-    printf("Product in order:\n");
-    printf("%s - $%.2f\n\n", orders[order_num].product.name, orders[order_num].product.price);
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tName: %s\n", orders[order_num].username);
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tOrder ID: %ld\n", orders[order_num].order_id);
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tProduct in order:\n");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\t%s - $%.2f\n\n", orders[order_num].product.name, orders[order_num].product.price);
 }
 
 void search_order() {
     char search_id[50];
-    printf("Enter Order ID or Username: ");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tEnter Order ID or Username: ");
     scanf("%s", search_id);
     if(isdigit(search_id[0])) {
         long order_id = atol(search_id);
-        printf("\nOrders found:\n\n");
+        printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tOrders found:\n\n");
         for(int i=0; i<order_count; i++) {
             if(orders[i].order_id == order_id) {
                 display_order(i);
@@ -314,7 +375,7 @@ void search_order() {
         }
     } else {
         int flag = 0;
-        printf("\nOrders Found:\n\n");
+        printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tOrders Found:\n\n");
         for(int i=0; i<order_count; i++) {
             for(int j=0; j<strlen(search_id); j++) {
                 if(search_id[j] == orders[i].username[j]) {
@@ -328,7 +389,7 @@ void search_order() {
             }
         }
         if(!flag) {
-            printf("No orders found!\n\n");
+            printf("\t\t\t\t\t\t\t\t\t\t\t\t\tNo orders found!\n\n");
         }
     }
 }
@@ -336,9 +397,9 @@ void search_order() {
 void edit_order(Order *order) {
     char new_name[50];
     int id;
-    printf("Enter new name: ");
+    printf("\t\t\t\t\t\t\t\t\t\t\t\t\tEnter new name: ");
     scanf("%s", new_name);
     strcpy(order->username, new_name);
     write_orders(orders);
-    printf("\nSuccessfully edited the order!\n\n");
+    printf("\n\t\t\t\t\t\t\t\t\t\t\t\t\tSuccessfully edited the order!\n");
 }
